d6f425ccedf61e5b63f7b30beaf8bcb9037d0d8f
UraduTsu <89968299+UraduTsu@users.noreply.github.com>
2026-01-04 17:29:02 +0100

    Add files via upload

--- app/src/main/assets/sw.js
+++ app/src/main/assets/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil((async () => {
-    const cache = await caches.open('nxa-player-v5-0');
+    const cache = await caches.open('nxa-player-v5-1');
     await cache.addAll(['./', './index.html', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png', './sw.js']);
     self.skipWaiting();
   })());
@@ -8,7 +8,7 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil((async () => {
     const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== 'nxa-player-v5-0').map(k => caches.delete(k)));
+    await Promise.all(keys.filter(k => k !== 'nxa-player-v5-1').map(k => caches.delete(k)));
     self.clients.claim();
   })());
 });
