a6e75b574058abdfa50cf6318c73f98703e5841a
UraduTsu <89968299+UraduTsu@users.noreply.github.com>
2026-01-04 19:01:41 +0100

    Add files via upload

--- app/src/main/assets/sw.js
+++ app/src/main/assets/sw.js
@@ -1,17 +1,19 @@
 self.addEventListener('install', (e) => {
   e.waitUntil((async () => {
-    const cache = await caches.open('nxa-player-v4-4');
-    await cache.addAll(['./', './index.html', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png', './sw.js']);
+    const cache = await caches.open('nxa-player-v4-5');
+    await cache.addAll(['./', './index.html', './app.js', './manifest.webmanifest', './favicon.ico', './apple-touch-icon.png', './icon-192.png', './icon-512.png', './icon-192-maskable.png', './icon-512-maskable.png', './sw.js']);
     self.skipWaiting();
   })());
 });
+
 self.addEventListener('activate', (e) => {
   e.waitUntil((async () => {
     const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== 'nxa-player-v4-4').map(k => caches.delete(k)));
+    await Promise.all(keys.filter(k => k !== 'nxa-player-v4-5').map(k => caches.delete(k)));
     self.clients.claim();
   })());
 });
+
 self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
     const cached = await caches.match(e.request);
